56e9fbf175d59049d88dd4723bc22ec511f7fb5e
Heiko Dreyer <mail@boxedfolder.com>
2011-05-25 19:37:26 +0200

    - Init commit

---
+++ UIView+Visuals.h
@@ -0,0 +1,34 @@
+//
+//  UIView+Visuals.h
+//  uiviewtmp
+//
+//  Created by Heiko Dreyer on 25.05.11.
+//  Copyright 2011 boxedfolder.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIView (Visuals)
+
+/*
+ *  Sets a corners with radius, given stroke size & color
+ */
+-(void)cornerRadius: (CGFloat)radius 
+         strokeSize: (CGFloat)size 
+              color: (UIColor *)color;
+
+/*
+ * Draws shadow with properties
+ */
+-(void)shadowWithColor: (UIColor *)color 
+                offset: (CGSize)offset 
+               opacity: (CGFloat)opacity 
+                radius: (CGFloat)radius;
+
+/*
+ * Removes from superview with fade
+ */
+-(void)removeFromSuperviewWithFadeDuration: (NSTimeInterval)duration;
+
+@end
---
+++ UIView+Visuals.m
@@ -0,0 +1,52 @@
+//
+//  UIView+Visuals.m
+//  uiviewtmp
+//
+//  Created by Heiko Dreyer on 25.05.11.
+//  Copyright 2011 boxedfolder.com. All rights reserved.
+//
+
+#import "UIView+Visuals.h"
+#import <QuartzCore/QuartzCore.h>
+
+
+@implementation UIView (Visuals)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+-(void)cornerRadius: (CGFloat)radius strokeSize: (CGFloat)size color: (UIColor *)color
+{
+    self.layer.cornerRadius = radius;
+    self.layer.borderColor = color.CGColor;
+    self.layer.borderWidth = size;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+-(void)shadowWithColor: (UIColor *)color 
+                offset: (CGSize)offset 
+               opacity: (CGFloat)opacity 
+                radius: (CGFloat)radius
+{
+    self.clipsToBounds = NO;
+    self.layer.shadowColor = color.CGColor;
+    self.layer.shadowOffset = offset;
+    self.layer.shadowOpacity = opacity;
+    self.layer.shadowRadius = radius;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+-(void)removeFromSuperviewWithFadeDuration: (NSTimeInterval)duration
+{
+    [UIView beginAnimations: nil context: nil];
+	[UIView setAnimationBeginsFromCurrentState: YES];
+	[UIView setAnimationDuration: duration];
+    [UIView setAnimationDelegate: self];
+    [UIView setAnimationDidStopSelector: @selector(removeFromSuperview)];
+    self.alpha = 0.0;
+	[UIView commitAnimations];	
+}
+
+@end
